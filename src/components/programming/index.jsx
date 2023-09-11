bb4789b335740fe77045cffd2dabcc5ff4b90b87
rafaelajaques <rafaelajaques@yahoo.com.br>
2023-09-11 19:34:00 -0300

    Atualização da grade horária

--- src/components/programming/index.jsx
+++ src/components/programming/index.jsx
@@ -1,7 +1,4 @@
-/* eslint-disable react/jsx-no-undef */
-/* eslint-disable react/jsx-no-comment-textnodes */
-/* eslint-disable no-undef */
-// import Container from "./style";
+import React, { useEffect, useState } from "react";
 import { Container } from "./style";
 import Carousel from "react-elastic-carousel";
 import {
@@ -10,60 +7,117 @@ import {
   AccordionDetails,
   Typography,
 } from "@mui/material";
-// import Accordion from "@mui/material/Accordion";
-// import AccordionSummary from "@mui/material/AccordionSummary";
-// import AccordionDetails from "@mui/material/AccordionDetails";
-// import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import axios from "axios";
+import { format, addDays } from "date-fns";
+
+const url = "http://localhost:3000/programmes/1337?";
+
+function AoVivo(startTime, endTime) {
+  const timestampAtual = Math.floor(Date.now() / 1000);
+
+  if (timestampAtual >= startTime && timestampAtual <= endTime) {
+    return "Ao Vivo";
+  } else {
+    return "";
+  }
+}
+
+async function Atualizar(dataFormatada) {
+  try {
+    axios.defaults.baseURL = "http://localhost:3000";
+    axios.defaults.headers.post["Content-Type"] =
+      "application/json;charset=utf-8";
+    axios.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
+
+    const newUrl = `${url}&date=${dataFormatada}`;
+
+    const response = await axios.get(newUrl);
+
+    console.log(response);
+
+    return response;
+  } catch (error) {
+    console.error("Erro na atualização:", error);
+    throw error; // Lança o erro para que possa ser tratado no chamador da função
+  }
+}
 
 function Programming(props) {
-  const dataList = props.data;
-  debugger;
+  const [programs, setPrograms] = useState([]);
+  const [dados, setDados] = useState(null);
+  const [currentPage, setCurrentPage] = useState(2);
+
+  useEffect(() => {
+    const dataFormatada = dias[currentPage - 1].dtString;
+
+    Atualizar(dataFormatada)
+      .then((response) => {
+        setDados(response.data);
+        setPrograms(response.data);
+        // dados = response.data;
+      })
+      .catch((error) => {
+        // Lide com erros aqui
+      });
+  }, [currentPage]);
+
+  const dias = [];
+  const numDias = 3;
+
+  for (let i = 0; i < numDias; i++) {
+    const date = addDays(new Date(), i - 1);
+    const formattedDate = format(date, "yyyy-MM-dd"); // Formate a data como "ano-mês-dia"
+
+    dias.push({ id: i + 1, dtString: formattedDate });
+  }
+
   return (
-    <Container>
-      <Carousel>
-        <div>
-          <p>SEX, 08 DE SETEMBRO</p>
-        </div>
-        <div>
-          <p>SÁB, 09 DE SETEMBRO</p>
-        </div>
-        <div>
-          <p>DOM, 10 DE SETEMBRO</p>
-        </div>
-        {/* {items.map((item) => (
-          <div key={item.id}>{item.title}</div>
-        ))} */}
-      </Carousel>
-      {dataList &&
-        dataList.programme.entries.map((item, index) => (
-          <Accordion className="accordion">
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              aria-controls="panel1a-content"
-              id="panel1a-header"
-            >
-              <Typography className="program">
-                <img
-                  src={item.custom_info.Graficos.LogoURL}
-                  alt="Logo do Jornal Hoje"
-                />
-                <p>13:25</p>
-                <p>{item.title}</p>
-              </Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography className="content">
-                <img src={item.custom_info.Graficos.ImagemURL} alt="" />
-                <p>
-                  Os destaques do dia no Brasil e no mundo, com apresentação de
-                  César Tralli.
-                </p>
-              </Typography>
-            </AccordionDetails>
-          </Accordion>
-        ))}
-    </Container>
+    <div>
+      <Container>
+        <Carousel
+          initialActiveIndex={currentPage - 1}
+          onChange={(currentItem, pageIndex) => setCurrentPage(pageIndex + 1)}
+        >
+          {dias.map((item, index) => (
+            <div key={item.id} initialActiveIndex={1}>
+              <p>{item.dtString}</p>
+            </div>
+          ))}
+        </Carousel>
+        {dados &&
+          dados.programme.entries.map((item, index) => (
+            <Accordion className="accordion" key={index}>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography className="program">
+                  <img
+                    src={item.custom_info.Graficos.LogoURL}
+                    alt="Logo do Jornal Hoje"
+                  />
+                  <p>
+                    {item.human_start_time.substring(0, 8)}{" "}
+                    {AoVivo(item.start_time, item.end_time)}
+                  </p>
+                  <p>{item.title}</p>
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography className="content">
+                  <img src={item.custom_info.Graficos.ImagemURL} alt="" />
+                  <p>
+                    Os destaques do dia no Brasil e no mundo, com apresentação
+                    de César Tralli.
+                  </p>
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+          ))}
+      </Container>
+    </div>
   );
 }
 
